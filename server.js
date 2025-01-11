686b410d9b80d32dac210b70d56db404d7c21ba5
Abdul Hanan <hananbhatti440@gmail.com>
2025-01-12 02:31:59 +0500

    Add Company Details Endpoint

--- server.js
+++ server.js
@@ -28,9 +28,30 @@ app.get("/api3/:param", (req, res) => {
 });
 
 // Environment-sensitive route
-app.get("/secure", (req, res) => {
-  const apiKey = process.env.API_KEY;
-  res.json({ message: "Secure route accessed", apiKey });
+app.get("/company-detail/:param", (req, res) => {
+  const uid = req.params.param;
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://www.zefix.admin.ch/ZefixPublicREST/api/v1/company/uid/'.uid,
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': process.env.AUTHORIZATION_KEY, // Store the authorization key in your .env
+    },
+    data : {}
+  };
+
+  axios.request(config)
+    .then((response) => {
+         // Set CORS headers for the response
+      res.set('Access-Control-Allow-Origin', '*');  // Allow all domains
+      res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  // Allowed methods
+      res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
+      res.json(response.data); // Send the response data back to the client
+    })
+    .catch((error) => {
+      res.status(500).json({ errorr: error }); // Handle errors and send them as response
+    });
 });
 
 // Route 5: /search-company
@@ -57,9 +78,9 @@ app.post("/search-company", (req, res) => {
   axios.request(config)
     .then((response) => {
          // Set CORS headers for the response
-    res.set('Access-Control-Allow-Origin', '*');  // Allow all domains
-    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  // Allowed methods
-    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
+      res.set('Access-Control-Allow-Origin', '*');  // Allow all domains
+      res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  // Allowed methods
+      res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
       res.json(response.data); // Send the response data back to the client
     })
     .catch((error) => {
