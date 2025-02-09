87ca0156a15461d0d950107173282b87ca80e629
Abdul Hanan <hananbhatti440@gmail.com>
2025-02-09 17:19:15 +0500

    Update API's

--- server.js
+++ server.js
@@ -10,22 +10,6 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000; // Use PORT from .env or default to 3000
 app.use(cors()); 
-// Route 1: Handle GET requests for API 1
-app.get("/api1", (req, res) => {
-  res.json({ message: "This is API 1 response" });
-});
-
-// Route 2: Handle POST requests for API 2
-app.post("/api2", (req, res) => {
-  const data = req.body;
-  res.json({ message: "This is API 2 response", receivedData: data });
-});
-
-// Route 3: Handle dynamic routes (e.g., API 3 with a parameter)
-app.get("/api3/:param", (req, res) => {
-  const param = req.params.param;
-  res.json({ message: 'This is API 3 response for param: ', param });
-});
 
 // Environment-sensitive route
 app.get("/company-uid/:param", (req, res) => {
